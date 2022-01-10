97c9664ed87a130a6ba9b0885d9da6d37ef6cd66
edos1 <92737607+edos1@users.noreply.github.com>
2022-01-10 22:05:36 +0100

    Simulation Datensatz begonnen
    Habe schon mal den Dataframe angelegt und die ersten beiden Spalten erzeugt. Passt das so?

---
+++ Simulation Datensatz.R
@@ -0,0 +1,16 @@
+### Simulation des Datensatzes
+  
+  # Spalte mit ids
+    id <- 1:100
+    daten <- data.frame(id)
+  
+  # Spalte Alter
+    alter <- rnorm(100, mean = 25, sd = 2)
+    daten <- cbind(daten, alter)
+  
+  # Spalte Studienfach
+    studienfaecher <- c("Statistik", "Data Science", "Mathe", "Informatik")
+    studienfach <- sample(studienfaecher, size = 100, replace = TRUE, prob = c(6/17, 6/17, 4/17, 1/17))
+    daten <- cbind(daten, as.factor(studienfach) )
+
+    
