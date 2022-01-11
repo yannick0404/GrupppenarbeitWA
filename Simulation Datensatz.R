e46981b967de7f1c9e9b981cec4e37ac759c9305
Yannick <85198799+yannick0404@users.noreply.github.com>
2022-01-11 19:07:17 +0100

    Update Simulation Datensatz.R
    Ich habe set.seed() hinzugefügt, damit bei jeder Simulation die gleichen Daten erzeugt werden.

--- Simulation Datensatz.R
+++ Simulation Datensatz.R
@@ -5,11 +5,13 @@
     daten <- data.frame(id)
   
   # Spalte Alter
+    set.seed(2022)
     alter <- rnorm(100, mean = 25, sd = 2)
     daten <- cbind(daten, alter)
   
   # Spalte Studienfach
     studienfaecher <- c("Statistik", "Data Science", "Mathe", "Informatik")
+    set.seed(12)
     studienfach <- sample(studienfaecher, size = 100, replace = TRUE, prob = c(6/17, 6/17, 4/17, 1/17))
     daten <- cbind(daten, as.factor(studienfach) )
 
