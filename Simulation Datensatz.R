1d9211865a43c6c3e531b5a3b127083ffed04caa
yannick0404 <yannick.hilker@web.de>
2022-01-12 15:45:10 +0100

    Verteilung Interesse an Mathe
    Ich hab das Interesse an Mathe zunächst unabhängig vom Studienfach simuliert
    und den seed vereinheitlicht.

--- Simulation Datensatz.R
+++ Simulation Datensatz.R
@@ -1,18 +1,23 @@
 ### Simulation des Datensatzes
   
-  # Spalte mit ids
-    id <- 1:100
-    daten <- data.frame(id)
+# Spalte mit ids
+  id <- 1:100
+  daten <- data.frame(id)
   
-  # Spalte Alter
-    set.seed(2022)
-    alter <- rnorm(100, mean = 25, sd = 2)
-    daten <- cbind(daten, alter)
+# Spalte Alter
+  set.seed(1)
+  alter <- rnorm(100, mean = 25, sd = 2)
+  daten <- cbind(daten, alter)
   
-  # Spalte Studienfach
-    studienfaecher <- c("Statistik", "Data Science", "Mathe", "Informatik")
-    set.seed(12)
-    studienfach <- sample(studienfaecher, size = 100, replace = TRUE, prob = c(6/17, 6/17, 4/17, 1/17))
-    daten <- cbind(daten, as.factor(studienfach) )
+# Spalte Studienfach
+  studienfaecher <- c("Statistik", "Data Science", "Mathe", "Informatik")
+  set.seed(1)
+  studienfach <- sample(studienfaecher, size = 100, replace = TRUE, prob = c(6/17, 6/17, 4/17, 1/17))
+  daten <- cbind(daten, as.factor(studienfach) )
 
+# Interesse an Mathematik
+  interesse <- ordered(1:7)
+  set.seed(1)
+  daten$interesse_mathe <- sample(interesse, size = 100, replace = T)
+  
     
