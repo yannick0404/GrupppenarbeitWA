3ac28ef923d34bbbea3e15a49e8b39b6331b2ccd
yannick0404 <yannick.hilker@web.de>
2022-01-16 15:50:00 +0100

    Interesse an Informatik
    Ich habe das Interesse an Informatik
    entsprechend angepasst.

--- Simulation Datensatz.R
+++ Simulation Datensatz.R
@@ -19,13 +19,10 @@
   
   # Spalte Interesse an Mathematik
   interesse <- ordered(1:7)
-  sim_interesse <- function(i){
-    if(daten$studienfach[i] == "Mathe" ){
-      # Poisson Verteilt simulieren
-      return(sample(interesse, size = 1, prob = c(ppois(1,6), dpois(2,6),
-                                                  dpois(3,6), dpois(4,6),
-                                                  dpois(5,6), dpois(6,6), 
-                                                  dpois(7,6)+ 1-ppois(7,6)), 
+  sim_interessem <- function(i){
+      if(daten$studienfach[i] == "Mathe" ){
+        return(sample(interesse, size = 1, 
+                      prob = c(1/30, 1/30, 2/30, 5/30, 5/30, 9/30, 7/30), 
                       replace = TRUE))
       }
     if(daten$studienfach[i] == "Statistik" ){
@@ -46,8 +43,34 @@
   }
   
   set.seed(13)
-  daten$interesse_mathe <- sapply( 1:100, sim_interesse )
+  daten$interesse_mathe <- sapply( 1:100, sim_interessem )
+  
+  # Spalte Interesse an Informatik
+  sim_interessei <- function(i){
+    if(daten$studienfach[i] == "Mathe" ){
+      return(sample(interesse, size = 1, 
+                    prob = c(3/30, 4/30, 6/30, 7/30, 5/30, 3/30, 2/30), 
+                    replace = TRUE))
+    }
+    if(daten$studienfach[i] == "Statistik" ){
+      return(sample(interesse, size = 1,
+                    prob = c(1/30, 2/30, 5/30, 6/30, 7/30, 6/30, 3/30), 
+                    replace = TRUE))
+    }
+    if(daten$studienfach[i] == "Data Science" ){
+      return(sample(interesse, size = 1,
+                    prob = c(1/30, 2/30, 4/30, 6/30, 8/30, 6/30, 3/30), 
+                    replace = TRUE))
+    }
+    if(daten$studienfach[i] == "Informatik" ){
+      return(sample(interesse, size = 1,
+                    prob = c(1/30, 1/30, 1/30, 4/30, 9/30, 8/30, 6/30), 
+                    replace = TRUE))
+    }
+  }
+  
+  set.seed(16)
+  daten$interesse_info <- sapply( 1:100, sim_interessei )
   
 
-    
   
