dc0d80e2566358b47a35b7794115707eaae71893
yannick0404 <yannick.hilker@web.de>
2022-01-26 15:05:11 +0100

    a
    Ich habe schon mal  ein Funktion, welche
    verschiedene deskriptive Statistiken für metrische Variablen berechnet, erstellt!
    Ladet eure Vorschläge auch gern hoch

--- Funktionen 1.R
+++ Funktionen 1.R
@@ -1,1 +1,10 @@
 ## Funktionen 1
+
+# a
+  deskrstatmetr <- function(x, na.rm = FALSE, ...){
+    if(!is.numeric(x)){
+      stop("x is not numeric")
+    }
+    return(c(summary(x, na.rm, ...), "sd" = sd(x, na.rm, ...)))
+  }
+
