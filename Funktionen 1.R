a211399e48c17ce7ee68c1a9bb7b1df8d564e14d
edos1 <92737607+edos1@users.noreply.github.com>
2022-01-29 15:12:25 +0100

    Ergänzungen zu a und b
    Habe in a noch das getrimmte Mittel ergänzt. Die Summe in b hab ich entfernt, da man die ja nicht auf kategorielle Varaiblen anwenden kann und noch den Modalwert und die Anzahl hinzugefügt. Habe auch schon mal die Umrisse für die restlichen Funktionen geschrieben.

--- Funktionen 1.R
+++ Funktionen 1.R
@@ -1,18 +1,44 @@
 ## Funktionen 1
 
 # a
-  deskrstatmetr <- function(x, na.rm = FALSE, ...){
-    if(!is.numeric(x)){
-      stop("x is not numeric")
-    }
-    return(c(summary(x, na.rm, ...), "sd" = sd(x, na.rm, ...)))
+deskrstatmetr <- function(x, na.rm = FALSE, ...){
+  if(!is.numeric(x)){
+    stop("x is not numeric")
   }
+  return(list(summary(x, na.rm, ...), "sd" = sd(x, na.rm, ...), 
+              "10%-getrimmtes Mittel" = mean(x, trim = .1)))
+}
 
 # b
-# Mache ich spÃ¤ter fertig
+# Mache ich später fertig
 
-diskrstatkat <- function(x){
-  return(list(Summe = sum(x)))
+deskrstatkat <- function(x){
+  if(!is.factor(x)){
+    stop("x is not a factor")
+  }
+  return( list( Anzahl = length(x), "Modalwert" = max(table(x)) ) )
 }
+
+# c
+
+deskrstatbivar_kat <- function(x, y){
   
+}
+
+# d
+
+deskrstatbivar_metr_dich <- function(x, y){
   
+}
+
+# e
+
+kategoriesierung <- function(x){
+  
+}
+
+# f
+
+visualisierung <- function(a, b, c, d){
+  
+}
