14581d90d2324cd0ec9a08151f9de65865c894b0
TimonM1705 <94565332+TimonM1705@users.noreply.github.com>
2022-02-17 13:59:07 +0100

    Update Funktionen 2.R

--- Funktionen 2.R
+++ Funktionen 2.R
@@ -3,3 +3,10 @@
 quants <- function(x, probs){
   return(as.vector(quantile(x, type = 1, probs = probs)))
 }
+
+hilfe_vis <- function(x){
+if(!is.factor(x)){
+  stop("x is not a factor")
+}
+barplot(table(x))
+}
